16ec308289fede60ffdaada2195f570dd70abbdd
tasmiyahh <tasmiyahhussainn99@gmail.com>
2022-11-11 16:57:17 -0800

    new

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -8,40 +8,46 @@ import { faAppleWhole, faCoffee } from '@fortawesome/free-solid-svg-icons'
 const App = () => {
 
   return (
-    <div className="main">
-      <div className="img">
-        <img src={spectacles} height="250px" alt="" />
-      </div>
+  <div className="head">
+      <div className="main">
+        <div className="img">
+          <img src={spectacles} height="250px" alt="" />
+        </div>
+
+     
 
       <div className="para">
-        <h2>
-          <pre>
-            Have the courage to follow your <br />
-            heart and intution
-          </pre>
-        </h2>
+        <h3>
+
+          Have the courage to follow your
+          heart and intution
+
+        </h3>
         <p className="para1">
-          
-            steve job was one of the most respected enterpreneus 
-            and inventors of his time . In 1976 he founded Apple 
-            together with his friend.  They  created their first 
-            computer . the Apple I. in  the garage of job's parents
-         
+
+          steve job was one of the most respected enterpreneus
+          and inventors of his time . In 1976 he founded Apple
+          together with his friend.  They  created their first
+          computer . the Apple I. in  the garage of job's parents
+
         </p>
-        <p>
-       <span className="icon"><FontAwesomeIcon icon={faAppleWhole} size="50px" /></span> 
-       <span className="title">
-           <b>   Steve Jobs</b>
-           <br />
-           <span className="date">    11 nov 2022</span>
-        
-          
-       
-       </span>
+        <p className="title">
+          <span className="icon"><FontAwesomeIcon icon={faAppleWhole} size="50px" /></span>
+          <span className="name">
+            Steve Jobs <br />
+            11 nov 2022</span>
+
+
+
+
         </p>
-      
+
       </div>
-    </div>
+
+      </div>
+
+      </div>
+    
   )
 }
 
